9f903bfa7decdbd5f1027324be15a2143d840180
Clelio De Souza <clelio.desouza@aimia.com>
2017-04-17 14:34:54 +0100

    add homepage and header views

--- app/app.js
+++ app/app.js
@@ -1,6 +1,74 @@
-const myNinjaApp = angular.module('myNinjaApp', []);
+const myNinjaApp = angular.module('myNinjaApp', ['ngRoute']);
 
-myNinjaApp.controller('NinjaController', ['$scope', function($scope){
+myNinjaApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'views/home.html',
+            controller: 'HomepageController'
+        })
+        .when('/directory', {
+            templateUrl: 'views/directory.html',
+            controller: 'NinjaController'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        })
+    
+}]);
+
+myNinjaApp.directive('firstNinja', function(){
+    return {
+        template: 'First ninja is {{ninjas[0].name}}'
+    }
+});
+
+myNinjaApp.directive('secondNinja', function(){
+    return {
+        restrict: 'E',
+        scope: {
+          prefix: '=',
+          ninjas: '='
+        },
+        template: 'Second ninja is {{prefix}} {{ninjas[1].name}}'
+    }
+});
+
+myNinjaApp.directive('randomNinja', [function() {
+
+    return {
+        restrict: 'E',
+        scope: {
+            ninjas: '=',
+            title: '='
+        },
+        templateUrl: 'views/random.html',
+        // template: '<img ng-src="{{ninjas[0].thumb}}"/>',
+        controller: function ($scope) {
+            $scope.random = Math.floor(Math.random() * 4);
+        }
+    }
+}]);
+
+myNinjaApp.service('httpFetcher', ['$rootScope', '$http', function ($rootScope, $http) {
+    var fetchNinjas = function() {
+        return $http.get('data/ninjas.json');
+    };
+
+    return {
+        getNinjas: fetchNinjas
+    };
+}]);
+
+myNinjaApp.controller('HomepageController', ['$scope', 'httpFetcher', function ($scope, httpFetcher) {
+    httpFetcher.getNinjas()
+        .then(function(res){
+            $scope.ninjas = res.data;
+        }, function() {
+            $scope.error = 'unable to get the ponies';
+        });
+}]);
+
+myNinjaApp.controller('NinjaController', ['$scope', 'httpFetcher', function($scope, httpFetcher){
 
     $scope.removeNinja = function(ninja) {
         // $scope.ninjas = $scope.ninjas.filter(n => !(n === ninja));
@@ -21,29 +89,12 @@ myNinjaApp.controller('NinjaController', ['$scope', function($scope){
         $scope.newNinja.rate = "";
     };
 
-    $scope.ninjas = [
-        {
-            name: "yoshi",
-            belt: "green",
-            rate: 30,
-            available: true,
-            thumb: "content/img/yoshi.jpg"
-        }, {
-            name: "crystal",
-            belt: "yellow",
-            rate: 50,
-            available: true
-        }, {
-            name: "ryu",
-            belt: "orange",
-            rate: 20,
-            available: false
-        }, {
-            name: "shaun",
-            belt: "black",
-            rate: 100,
-            available: true
-        }
-    ];
+    httpFetcher.getNinjas()
+        .then(function(res){
+            $scope.ninjas = res.data;
+        }, function() {
+            $scope.error = 'unable to get the ponies';
+        });
+    // console.log(angular.toJson($scope.ninjas));
 
 }]);
